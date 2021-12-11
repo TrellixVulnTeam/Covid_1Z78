373d91a1c8d2953b758d4eb25bac6999b9302116
EricChukoltyansky <kazohelmet@gmail.com>
2021-12-11 13:41:46 +0200

    showing countries

--- index.js
+++ index.js
@@ -4,37 +4,36 @@ const api = {
   covid: "https://corona-api.com/countries/af",
 };
 
-const regionInfo = {};
-const coronaInfo = {};
+const regionInformation = {};
+const covidInformation = {};
 
-let currentRegionInfo = [];
+let currentRegionInformation = [];
 
 async function fetchCountriesByregion() {
   const { data } = await axios.get(
     api.proxy + `https://restcountries.herokuapp.com/api/v1`
   );
   data.forEach((el) => {
-    regionInfo[el.region]
-      ? (regionInfo[el.region] += `*${el.name.common}`)
-      : (regionInfo[el.region] = `*${el.name.common}`);
+    regionInformation[el.region]
+      ? (regionInformation[el.region] += `*${el.name.common}`)
+      : (regionInformation[el.region] = `*${el.name.common}`);
   });
 
-  console.log("region info", regionInfo);
+  // console.log("region info", regionInformation);
 
-  for (let [region, country] of Object.entries(regionInfo)) {
-    // make values as arr
-    regionInfo[region] = country.split("*");
+  for (let [region, country] of Object.entries(regionInformation)) {
+    regionInformation[region] = country.split("*");
   }
 }
 
-async function fetchCovidInfoByCountry() {
+async function fetchCovidInfoByCountryrmation() {
   let dataCovid = await fetch(`https://corona-api.com/countries`);
   dataCovid = await dataCovid.json();
 
-  console.log("data covid", dataCovid);
+  // console.log("data covid", dataCovid);
 
   dataCovid.data.forEach((el) => {
-    coronaInfo[el.name] = [
+    covidInformation[el.name] = [
       el.latest_data.confirmed,
       el.latest_data.recovered,
       el.latest_data.critical,
@@ -46,22 +45,24 @@ async function fetchCovidInfoByCountry() {
 }
 
 fetchCountriesByregion().then(() => {
-  fetchCovidInfoByCountry();
+  fetchCovidInfoByCountryrmation();
 });
 
 function getInfoByregion(region) {
-  currentRegionInfo = [];
-  regionInfo[region].forEach((el) => {
-    currentRegionInfo.push([el, coronaInfo[el]]);
+  currentRegionInformation = [];
+  regionInformation[region].forEach((el) => {
+    currentRegionInformation.push([el, covidInformation[el]]);
   });
 
-  console.log(currentRegionInfo);
+  // console.log(currentRegionInformation);
 
-  currentRegionInfo = Object.values(currentRegionInfo).filter((el) => {
-    if (el[1]) {
-      return true;
-    } else false;
-  });
+  currentRegionInformation = Object.values(currentRegionInformation).filter(
+    (el) => {
+      if (el[1]) {
+        return true;
+      } else false;
+    }
+  );
 }
 
 let chart = document.getElementById("myChart");
@@ -142,7 +143,7 @@ function updateChartType() {
     type: document.getElementById("chartType").value,
     data: newChart,
   });
-  console.log(newChart);
+  // console.log(newChart);
 }
 
 function removeData(chart) {
@@ -161,10 +162,10 @@ function addData(chart, label, data) {
   chart.update();
 }
 
-// function showChart() {
-//   // document.querySelector(".chart-container").style.visibility = "visible";
-//   // document.querySelector(".country-info").style.visibility = "hidden";
-// }
+function showChart() {
+  document.querySelector(".chart-container").style.visibility = "visible";
+  document.querySelector(".country-info").style.visibility = "hidden";
+}
 
 let currentDataType = 0;
 
@@ -172,17 +173,17 @@ function updateDataByRegion(region) {
   getInfoByregion(region);
   removeData(myChart);
 
-  for (let info of Object.values(currentRegionInfo)) {
+  for (let info of Object.values(currentRegionInformation)) {
     addData(myChart, info[0], info[1][currentDataType]);
-    console.log(info[0]);
-    console.log(info[1]);
+    // console.log(info[0]);
+    // console.log(info[1][currentDataType]);
   }
 }
 
 function updateDataType(dataType) {
   removeData(myChart);
   currentDataType = dataType;
-  for (let info of Object.values(currentRegionInfo)) {
+  for (let info of Object.values(currentRegionInformation)) {
     addData(myChart, info[0], info[1][dataType]);
   }
 }
@@ -213,60 +214,107 @@ function buttonsSwitch(event) {
   switch (event.target.innerText) {
     case "Asia":
       currentRegion = "Asia";
+      showChart();
       updateDataByRegion(currentRegion);
       updateHeadline(currentRegion, currentDataType);
-      console.log(currentRegion);
+      updateRegionCountryNames(currentRegion);
+
       break;
     case "Europe":
       currentRegion = "Europe";
-
+      showChart();
       updateDataByRegion(currentRegion);
       updateHeadline(currentRegion, currentDataType);
-      console.log(currentRegion);
+      updateRegionCountryNames(currentRegion);
+
       break;
     case "Africa":
       currentRegion = "Africa";
-
+      showChart();
       updateDataByRegion(currentRegion);
       updateHeadline(currentRegion, currentDataType);
-      console.log(currentRegion);
+      updateRegionCountryNames(currentRegion);
+
       break;
     case "Americas":
       currentRegion = "Americas";
+      showChart();
       updateDataByRegion(currentRegion);
       updateHeadline(currentRegion, currentDataType);
-      console.log(currentRegion);
+      updateRegionCountryNames(currentRegion);
+
       break;
     case "Oceania":
       currentRegion = "Oceania";
-
+      showChart();
       updateDataByRegion(currentRegion);
       updateHeadline(currentRegion, currentDataType);
-      console.log(currentRegion);
+      updateRegionCountryNames(currentRegion);
+      // console.log(currentRegion);
       break;
     case "Confirmed":
-      // showChart();
+      showChart();
       updateDataType(0);
       updateHeadline(currentRegion, 0);
-      console.log(currentRegion);
+      // console.log(currentRegion);
       break;
     case "Deaths":
-      // showChart();
+      showChart();
       updateDataType(3);
       updateHeadline(currentRegion, 3);
-      console.log(currentRegion);
+      // console.log(currentRegion);
       break;
     case "Recovered":
-      // showChart();
+      showChart();
       updateDataType(1);
       updateHeadline(currentRegion, 1);
-      console.log(currentRegion);
+      // console.log(currentRegion);
       break;
     case "Critical":
-      // showChart();
+      showChart();
       updateDataType(2);
       updateHeadline(currentRegion, 2);
-      console.log(currentRegion);
+      // console.log(currentRegion);
       break;
   }
 }
+
+function updateRegionCountryNames(region) {
+  let currentRegionCountries = [];
+  regionInformation[region].forEach((el) => {
+    currentRegionCountries.push(el);
+  });
+
+  document.querySelector(".countries-of-region").innerHTML = ""; // removing prior countries
+  for (let country of currentRegionCountries) {
+    document
+      .querySelector(".countries-of-region")
+      .insertAdjacentHTML(
+        "beforeend",
+        `<p class='${country}'>${country}</p><br><br>`
+      );
+    console.log("something");
+  }
+}
+
+document.querySelector(".countries").addEventListener("click", (el) => {
+  // show info for each country
+  document.querySelector(".chart-container").style.visibility = "hidden"; // makes chart hidden and country info visible
+  document.querySelector(".country-info").style.visibility = "visible";
+
+  let countryInfo = document.querySelector(".country-info");
+  if (el.target.innerText.length < 40) {
+    // prevents updating inner text to all countries in region (when target is the all div)
+    countryInfo.querySelector(
+      "h1"
+    ).innerText = `${el.target.innerText} Covid-19 Info:`;
+  }
+  console.log("2");
+
+  let [confirmed, deaths, recovered, critical] =
+    countryInfo.querySelectorAll("p"); // selecting all p to use for info
+  confirmed.innerText = covidInformation[el.target.innerText][0];
+  deaths.innerText = covidInformation[el.target.innerText][3];
+  recovered.innerText = covidInformation[el.target.innerText][1];
+  critical.innerText = covidInformation[el.target.innerText][2];
+});
